9a29ab7eeb2e2f5a4fba716f3ae873bcf5fd817f
839 <8398a7@gmail.com>
2015-07-30 00:50:05 +0900

    スケジュールを管理するコントローラを作成

---
+++ app/controllers/admin/schedules_controller.rb
@@ -0,0 +1,47 @@
+class Admin::SchedulesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_user!
+  before_action :load_schedule, except: [:index, :new, :create]
+
+  def index
+    @search = Schedule.search(params[:q])
+    @schedules = @search.result.page(params[:page]).per(20)
+  end
+
+  def edit
+    render :show_modal_form
+  end
+
+  def update
+    @schedule.update(schedule_params)
+    render :reload
+  end
+
+  def new
+    render :show_modal_form
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    @schedule.save
+    render :reload
+  end
+
+  def destroy
+    @schedule.destroy
+    render :reload
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(
+      :role
+    )
+  end
+
+  def load_schedule
+    return unless params[:id]
+    @schedule = Schedule.find_by(id: params[:id])
+  end
+end
